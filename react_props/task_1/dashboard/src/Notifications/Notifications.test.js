d6a5d593bfee26c5e0359fb1a27e257043780917
Caaronbll <aaronbll46@gmail.com>
2024-05-11 16:24:43 -0500

    adding tests for props

---
+++ react_props/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,24 @@
+import { shallow } from "enzyme";
+import React from "react";
+import Notifications from "./Notifications";
+
+describe("Notifications", () => {
+    // doesnt crash
+    test("renders without crashing", () => {
+        const wrapper = shallow(<Notifications />);
+        expect(wrapper.exists()).toBe(true);
+    });
+    // renders items
+  test("renders items", () => {
+    const wrapper = shallow(<Notifications />);
+    wrapper.update();
+    expect(wrapper.find("li")).toHaveLength(3);
+  });
+  // renders text
+  test("renders text", () => {
+    const text = "Here is the list of notifications";
+    const wrapper = shallow(<Notifications />);
+    wrapper.update();
+    expect(wrapper.find(".Notifications p").text()).toBe(text);
+  });
+});
